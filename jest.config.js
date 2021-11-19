fc0572e62b379c41c91bea775b677c04aefbf1ca
Jon Spaeth <jspaeth@appareo.com>
2021-11-19 12:42:03 -0600

    updating jest config to support restructured project

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
-  roots: ['<rootDir>/test', '<rootDir>/src'],
+  roots: ['<rootDir>/src'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
